efcfc648a8df885b635145a3e4b6573c6802f545
Daniil-Sokolov <daniil.sok2017@yandex.ru>
2018-01-28 15:11:32 +0300

    5

--- assets/common.js
+++ assets/common.js
@@ -1,24 +1,13 @@
-$(function(){
-      
-      // pjax
-      $(document).pjax('a', '#pjax-container', {fragment: '#pjax-container'});
-    })
-
-
-
-
-$(document).on('ready pjax:end', function(event) {
-
-
-
-$(document).ready(function() { 
-
-   $.getScript('/assets/ytplayer/jquery.mb.ytplayer.min.js');
-
-   $.getScript('/assets/theme/js/script.js');
-
-})
-
-})
-
-
+$(function() {
+	$(document).pjax("a", "#pjax-container", {
+		fragment: "#pjax-container"
+	})
+});
+$(document).on("ready pjax:end", function(a) {
+	$(document).ready(function() {
+		$.getScript("/assets/web/assets/jquery/jquery.min.js");
+		$.getScript("/assets/ytplayer/jquery.mb.ytplayer.min.js");
+		$.getScript("https://s.ytimg.com/yts/jsbin/www-widgetapi-vflcZyLnu/www-widgetapi.js");
+		$.getScript("/assets/theme/js/script.js")
+	})
+});
