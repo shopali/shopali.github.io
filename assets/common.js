f023b4fbc61539f30f81592cd51d812ed3d1db9b
Daniil-Sokolov <daniil.sok2017@yandex.ru>
2018-01-28 17:16:22 +0300

    5

--- assets/common.js
+++ assets/common.js
@@ -5,9 +5,7 @@ $(function() {
 });
 $(document).on("ready pjax:end", function(a) {
 	$(document).ready(function() {
-		$.getScript("/assets/web/assets/jquery/jquery.min.js");
 		$.getScript("/assets/ytplayer/jquery.mb.ytplayer.min.js");
-		$.getScript("https://s.ytimg.com/yts/jsbin/www-widgetapi-vflcZyLnu/www-widgetapi.js");
 		$.getScript("/assets/theme/js/script.js")
-	})
+	});
 });
